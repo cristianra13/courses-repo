cf234719dbf17fe3afdff043d064c0573b8e154a
Cristian Real <tin936@hotmail.com>
2022-06-11 17:36:11 -0500

    Added new changes to consumer, such as concurrency, override methods to configuration, etc

--- WorkspaceSpring/courses/udemy/apache-kafka-using-springboot/library-events-consumer/library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ WorkspaceSpring/courses/udemy/apache-kafka-using-springboot/library-events-consumer/library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,12 +1,38 @@
 package com.learnkafka.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
 
+  private final KafkaProperties properties;
+
+  public LibraryEventsConsumerConfig(KafkaProperties properties) {
+    this.properties = properties;
+  }
+
+  @Bean
+  ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+    ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+    ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+    ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    configurer.configure(factory, kafkaConsumerFactory
+      .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
+    // Set multiples kafka listeners for the same application
+    factory.setConcurrency(3);
+    //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+    return factory;
+  }
 
 
 }
